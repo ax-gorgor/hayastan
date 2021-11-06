743988cd492dddbfbc4ab1f3a34f624c5b9a3ace
ax-gorgor <80730395+ax-gorgor@users.noreply.github.com>
2021-11-05 22:53:54 -0300

    Add files via upload

---
+++ ja/javascript1303.js
@@ -0,0 +1,74 @@
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+var player;
+function onYouTubeIframeAPIReady() {
+   player = new YT.Player('player', {
+     height: '371',
+     width: '660',
+     videoId: 'Z-iiq_mekZM',   
+     playerVars: { 
+			
+            'showinfo': 0,
+			'rel':0,
+			'fs':0
+  		},
+       
+     
+     events: {
+       'onStateChange': onPlayerStateChange
+     }
+   });
+ }
+
+var titleName = 'Esh'
+
+var tiempoDeTitulo = 0
+
+function colorTitle(){
+
+  var tiempoDeVideo = player.getCurrentTime();
+
+  if(tiempoDeTitulo > 5) {
+    titleName.style.color = '#002395';  
+  } 
+
+
+}
+
+function onPlayerStateChange(event) {
+
+  if (event.data == YT.PlayerState.PLAYING) {
+  
+   setInterval(() => {
+     tiempoDeTitulo = player.getCurrentTime();
+   }, 100);
+  }
+}
+
+ 
+setInterval(() => {
+ colorText()
+}, 100);
+
+
+  
+ function onPlayerStateChange(event) {
+
+   if (event.data == YT.PlayerState.PLAYING) {
+   
+    setInterval(() => {
+      tiempoDeTitulo = player.getCurrentTime();
+    }, 100);
+   }
+ }
+
+ 
+setInterval(() => {
+  colorText()
+}, 100);
