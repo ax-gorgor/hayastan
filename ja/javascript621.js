a49ae36eda26b4e639aee88290bbe6470e4d190d
ax-gorgor <80730395+ax-gorgor@users.noreply.github.com>
2022-01-04 20:29:47 -0300

    Add files via upload

--- ja/javascript621.js
+++ ja/javascript621.js
@@ -37,6 +37,14 @@ function onYouTubeIframeAPIReady() {
  var a6 = document.getElementById('a6');
  var a7 = document.getElementById('a7');
  var a8 = document.getElementById('a8');
+ var a9 = document.getElementById('a9');
+ var a10 = document.getElementById('a10');
+ var a11 = document.getElementById('a11');
+ var a12 = document.getElementById('a12');
+ var a13 = document.getElementById('a13');
+ var a14 = document.getElementById('a14');
+ var a15 = document.getElementById('a15'); 
+  
 
  function colorText() {
 
@@ -54,6 +62,337 @@ function onYouTubeIframeAPIReady() {
   if(tiempoDeVideo > 6 ) {
     a0.style.color = 'white'; //rojo    //1
   } 
+
+
+  if(tiempoDeVideo > 6) {
+    a1.style.color = 'black';  
+    a2.style.color = 'grey';
+    a3.style.color = 'grey';
+    a4.style.color = 'grey';
+    a5.style.color = 'grey';
+    a6.style.color = 'grey';
+    a7.style.color = 'grey';
+    a8.style.color = 'grey';
+    a9.style.color = 'grey';
+    a10.style.color = 'grey';
+    a11.style.color = 'grey';
+    a12.style.color = 'grey';
+    a13.style.color = 'grey';
+    a14.style.color = 'grey';
+
+ } 
+
+ if(tiempoDeVideo > 16) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'black';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 27) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'black';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 38) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'black';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 48) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'black';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 58) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'black';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 69) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'black';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 80) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'black';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 90) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'black';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 101) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'black';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 111) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'black';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 122) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'black';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 131) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'black';
+  a14.style.color = 'grey';
+
+} 
+
+if(tiempoDeVideo > 131) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'black';
+  a14.style.color = 'grey';
+
+}
+
+if(tiempoDeVideo > 142) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'black';
+
+}
+
+if(tiempoDeVideo > 153) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+
+}
+
+if(tiempoDeVideo > 154) {
+  a1.style.color = 'grey';  
+  a2.style.color = 'grey';
+  a3.style.color = 'grey';
+  a4.style.color = 'grey';
+  a5.style.color = 'grey';
+  a6.style.color = 'grey';
+  a7.style.color = 'grey';
+  a8.style.color = 'grey';
+  a9.style.color = 'grey';
+  a10.style.color = 'grey';
+  a11.style.color = 'grey';
+  a12.style.color = 'grey';
+  a13.style.color = 'grey';
+  a14.style.color = 'grey';
+  a15.style.color = 'black';
+
+}
+ 
+if(tiempoDeVideo > 159) {
+  a1.style.color = 'black';  
+  a2.style.color = 'black';
+  a3.style.color = 'black';
+  a4.style.color = 'black';
+  a5.style.color = 'black';
+  a6.style.color = 'black';
+  a7.style.color = 'black';
+  a8.style.color = 'black';
+  a9.style.color = 'black';
+  a10.style.color = 'black';
+  a11.style.color = 'black';
+  a12.style.color = 'black';
+  a13.style.color = 'black';
+  a14.style.color = 'black';
+  a15.style.color = 'white';
+
+}
+
+
+ 
+
 
 }
 
